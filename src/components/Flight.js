c5b4330e8e5c47eece9b9e619768aa84e546c5e4
Rakesh <2100032231@kluniversity.in>
2023-02-13 21:43:32 +0530

    All changes 13/02/2023

---
+++ src/components/Flight.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Flight.css'
+export default function a() {
+  return (
+	<div className="f_container">
+		<form action="#">
+			<h1>Book Your Flight</h1>
+      <label for="source" id='cls2'>Source:</label>
+			<input type="text" placeholder="Enter Source" />
+      <label for="destination" id='cls3'>Destination:</label>
+			<input type="email" placeholder="Enter Destination" />
+      <label for="destination" id='cls4'>Type of Travel:</label>
+      <select name="cars" id="Type of Travel">
+      <option value="volvo">Business Class</option>
+      <option value="saab">Economy Class</option>
+      <option value="mercedes">First Class</option>
+</select>
+      <label for="Journey Date" id='cls'>Date of Journey:</label>
+      <input type="date" id="doj" name="doj"></input>
+      <label for="Journey Date" id='cls1'>Date of Return:</label>
+      <input type="date" id="doj" name="doj"></input>
+      <label for="destination" id='cls5'>No of passengers:</label>
+      <input type="number" placeholder="Number of Passengers" />
+		</form>
+
+	</div>
+  )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Navbar.css';
+import logo from './media/logos.jpg'
+import { NavLink } from 'react-router-dom'
+export default function Navbar() {
+  return (
+    <>
+     <nav >
+      
+            <ul>
+                <img src={logo} alt="" id='logo'></img>
+                <NavLink to="/home" ><label>Home</label></NavLink>&emsp;&emsp;
+                <NavLink to="/flight" ><label>FLIGHTS</label></NavLink>&emsp;&emsp;
+                <NavLink to="/hotels" ><label>HOTELS</label></NavLink>&emsp;&emsp;
+                <NavLink to="/about"><label>About us</label></NavLink>&emsp;&emsp;  
+                <NavLink to="/login"><label>Login</label></NavLink>&emsp;&emsp;            
+            </ul>
+        </nav>
+    </>
+  )
+}
