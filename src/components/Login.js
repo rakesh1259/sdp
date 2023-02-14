d58278ec043120f5f3bb0dee203d72446fa1a716
Rakesh <2100032231@kluniversity.in>
2023-02-14 14:55:28 +0530

    login nzme

--- src/components/Login.js
+++ src/components/Login.js
@@ -53,7 +53,7 @@ export default function Login() {
 				<button className="ghost" id="signIn" onClick={handleon}>Sign In</button>
 			</div>
 			<div className="overlay-panel overlay-right">
-				<h1>Hello, Friend!</h1>
+				<h1>Hello, Traveller!</h1>
 				<p>Enter your personal details and start journey with us</p>
 				<button className="ghost" id="signUp" onClick={handleon1}>Sign Up</button>
 			</div>
