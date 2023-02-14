38815213ffd21869392158f33bb0f25f8c0dc82b
Rakesh <2100032231@kluniversity.in>
2023-02-14 14:32:29 +0530

    hotel page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Routes,Route} from 'react-router-dom'
 import Login from './components/Login';
 import Home from './components/Home';
 import Flight from './components/Flight';
+import Hotel from './components/Hotel';
 function App() {
   return (
     <>
@@ -12,6 +13,7 @@ function App() {
     <Route path='/home' element={<Home/>}/>
       <Route path='/login' element={<Login/>}/>
       <Route path='/flight' element={<Flight/>}/>
+      <Route path='/hotel' element={<Hotel/>}/>
     </Routes>
    </> 
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,9 +11,9 @@ export default function Navbar() {
                 <img src={logo} alt="" id='logo'></img>
                 <NavLink to="/home" ><label>Home</label></NavLink>&emsp;&emsp;
                 <NavLink to="/flight" ><label>FLIGHTS</label></NavLink>&emsp;&emsp;
-                <NavLink to="/hotels" ><label>HOTELS</label></NavLink>&emsp;&emsp;
+                <NavLink to="/hotel" ><label>HOTELS</label></NavLink>&emsp;&emsp;
                 <NavLink to="/about"><label>About us</label></NavLink>&emsp;&emsp;  
-                <NavLink to="/login"><label>Login</label></NavLink>&emsp;&emsp;            
+                <NavLink to="/login"><label>Login</label></NavLink>&emsp;&emsp;    
             </ul>
         </nav>
     </>
