60400cf931b1af4b40d4037cfbe59bc0031d4604
vinay1319 <vinay1319@gmail.com>
2023-02-22 14:11:04 +0530

    home page changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,48 @@
 import React from "react";
 import "./Home.css";
+// import m from "./media/mexico.jpg";
 export default function Home() {
   return (
     <div class="h_container">
-      <h1>OUR WEBSITE SERVICES</h1>
-      <p>
-        Flights, International Flights, Charter Flights, Hotels, International
-        Hotels, Homestays and Villas, Activities, Holidays In India,
-        International Holidays, Book Hotels From UAE, myBiz for Corporate
-        Travel, Book Online Cabs, Book Bus Tickets, Book Train Tickets, Cheap
-        Tickets to India, Book Flights From US, Book Flights From UAE, Trip
-        Planner, Gift Cards, Trip Money, Trip Ideas, Travel Blog, PNR Status,
-        MakeMyTrip Advertising{" "}
-      </p>
+<div class="h_card">
+  <div class="face face1">
+    <div class="content">
+      <span class="stars"></span>
+      <h2 class="java">Java</h2>
+      <p class="java">Java is a class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible.</p>
     </div>
+  </div>
+  <div class="face face2">
+  </div>
+</div>
+
+<div class="h_card">
+  <div class="face face1">
+    <div class="content">
+      <span class="stars"></span>
+      <h2 class="python">Python</h2>
+      <p class="python">Python is an interpreted, high-level and general-purpose programming language.</p>
+    </div>
+  </div>
+  <div class="face face2">
+    <h2>02</h2>
+  </div>
+</div>
+
+<div class="h_card">
+  <div class="face face1">
+    <div class="content">
+      <span class="stars"></span>
+      <h2 class="cSharp">C#</h2>
+      <p class="cSharp">C# is a general-purpose, multi-paradigm programming language encompassing static typing, strong typing, lexically scoped and component-oriented programming disciplines.</p>
+    </div>
+  </div>
+  <div class="face face2">
+    <h2>03</h2>
+  </div>
+</div>
+</div>
+
+
   );
 }
