7f8b483ca8889b43e4e0eb1317f89ee68ba304ee
Rakesh <2100032231@kluniversity.in>
2023-02-24 22:45:35 +0530

    lava

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,20 +9,13 @@ export default function Navbar() {
     <>
      <nav >
             <ul>
-<<<<<<< HEAD
                 <a href="/"><img src={logo} alt='' id='logo' /></a>
                 <NavLink to="/" ><label>Home</label></NavLink>&emsp;&emsp;
-=======
-              <div className='navbar'>
-                <img src={logo} alt="" id='logo'></img>
-                <NavLink to="/home" ><label>Home</label></NavLink>&emsp;&emsp;
->>>>>>> 2705f5cc50268d5ee52aef2401d97b0662eb9b3e
                 <NavLink to="/flight" ><label>FLIGHTS</label></NavLink>&emsp;&emsp;
                 <NavLink to="/hotel" ><label>HOTELS</label></NavLink>&emsp;&emsp;
                 <NavLink to="/booking"><label>Booking History</label></NavLink>&emsp;&emsp;  
                 <NavLink to="/about"><label>About us</label></NavLink>&emsp;&emsp;  
                 <NavLink to="/login"><label>Login</label></NavLink>&emsp;&emsp;  
-                </div>  
             </ul>
         </nav>
     </>
