7ebd2e2e39c19516566d6d12c00b69242bf67e43
Rakesh <2100032231@kluniversity.in>
2023-03-17 13:32:15 +0530

    done..

--- src/components/FResult.js
+++ src/components/FResult.js
@@ -1,7 +1,24 @@
 import React from 'react'
-
+import './fresult.css'
+import ai from './media/airindia.png'
+import e from './media/emirates.png'
 export default function FResult() {
   return (
-    <div>FResult</div>
+    <>
+    <div className='rcon'>
+    <div className="img">
+    <img src={ai} alt=""/>
+    </div>
+      <p>The winning logo was created by Predrag Stakić from Serbia.</p> 
+  </div>
+  <div className='rcon2'>
+  <div class="img">
+    <img src={e} alt=""/>
+    <div>
+      <p>The winning logo was created by Predrag Stakić from Serbia.</p>     
+    </div>
+  </div>
+  </div>
+  </>
   )
 }
